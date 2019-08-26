c43fa80e86374dd72f90081c2c89a8290dfddfd8
moliu <1946218941@qq.com>
2019-08-26 18:07:13 +0800

    bitmex ws update.

--- bitmex_websocket/bitmex_sustainable_websocket.go
+++ bitmex_websocket/bitmex_sustainable_websocket.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"os"
+	"time"
 )
 
 type Bitmex interface {
@@ -49,7 +50,91 @@ func (o *bitmex) WsConnect() {
 
 // ping
 func (o *bitmex) Ping() {
-	err := o.Ws.WriteMessage(websocket.TextMessage, []byte("ping"))
+	o.Ws.PingHandler()
+	err := o.Ws.WriteJSON("ping")
+	if err != nil {
+		fmt.Println("ping error: ", err)
+	}
+	time.Sleep(5 * time.Second)
+}
+
+// 订阅subscribe
+func (o *bitmex) Subscribe() {
+	if o.Channels == nil {
+		fmt.Println("no channels")
+		return
+	}
+	for _, channel := range o.Channels {
+		err := o.Ws.WriteJSON(*channel)
+		if err != nil {
+			fmt.Println("subscribe error: ", err)
+		}
+	}
+}
+
+// 读取消息
+func (o *bitmex) ReadMessage() {
+	for true {
+		msgType, msg, err := o.Ws.ReadMessage()
+		if err != nil {
+			fmt.Println("read message error: ", err)
+			break
+		}
+		if msgType == websocket.TextMessage {
+			data := string(msg)
+			fmt.Println("message: ", msgType, data)
+		} else if msgType == websocket.CloseMessage {
+			// 关闭当前websocket
+			err := o.Ws.Close()
+			if err != nil {
+				fmt.Println("close websocket error: ", err)
+			}
+			break
+		}
+
+	}
+
+}
+
+// depth funding Instrument消息
+func (o *bitmex) BmDepthFundingInstrumentWebsocket() {
+	bmSymbols := NewBitmexSymbol()
+	var depthArgs []string
+	var fundingArgs []string
+	var instrumentArgs []string
+	for _, symbol := range bmSymbols.BitmexSymbols {
+		depthCh := "orderBook10:" + symbol
+		fundingCh := "funding:" + symbol
+		instrumentCh := "instrument:" + symbol
+		depthArgs = append(depthArgs, depthCh)
+		fundingArgs = append(fundingArgs, fundingCh)
+		instrumentArgs = append(instrumentArgs, instrumentCh)
+	}
+	args := append(append(depthArgs, fundingArgs...), instrumentArgs...)
+	channel := BmWebsocketRequest{"subscribe", args}
+	o.Channels = append(o.Channels, &channel)
+}
+
+// trade quote kline消息
+func (o *bitmex) BmTradeQuoteKlineWebsocket() {
+	bmSymbols := NewBitmexSymbol()
+	bmCycles := NewBitmexCycle()
+	var tradeArgs []string
+	var quoteArgs []string
+	var klineArgs []string
+	for _, symbol := range bmSymbols.BitmexSymbols {
+		tradeCh := "trade:" + symbol
+		quoteCh := "quote:" + symbol
+		tradeArgs = append(tradeArgs, tradeCh)
+		quoteArgs = append(quoteArgs, quoteCh)
+		for _, cycle := range bmCycles.BitmexCycles {
+			klineCh := "tradeBin" + cycle + ":" + symbol
+			klineArgs = append(klineArgs, klineCh)
+		}
+	}
+	args := append(append(tradeArgs, quoteArgs...), klineArgs...)
+	channel := BmWebsocketRequest{"subscribe", args}
+	o.Channels = append(o.Channels, &channel)
 }
 
 type BmWebsocketRequest struct {
